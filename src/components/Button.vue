f7bc36f9146cccf7a4e5bdddccde5210ae91ab24
C-Jalloh <100509637+C-Jalloh@users.noreply.github.com>
2022-11-22 22:56:01 +0000

    this is the first commit im still working on a nice catch-phrase for my commit message

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+	<button @click="onClick()"
+	:style="{background:color}"
+	v-bind:class="btn"
+	>
+		{{text}}
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'Button',
+	props: {
+		text: {
+			type: String,
+			default: 'Add Task',
+			required: true
+		},
+		color: {
+			type: String,
+			default: 'green'
+		}
+	},
+	methods: {
+		onClick() {
+			this.$emit('click')
+		}
+	}
+};
+
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+    <h1>
+{{title}}
+    </h1>
+    <Button text="Add Task" color="green" />
+    
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: 'Task Tracker',
+            required: true
+        }
+    },
+    components: {
+        Button
+    }
+}
+</script>
+<style scoped>
+header{
+    /*background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 15px;*/
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+
+}
+</style>
